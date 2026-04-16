45414efd6e1ca7c97689c5c2c33c9ab2f5975805
Duong Hieu <dthieu2910@gmail.com>
2026-04-16 16:18:30 +0700

    Add files via upload

---
+++ src/main/java/com/auction/client/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.auction.client.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+// 1+1=2
+public class LoginController {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    public void handleLogin()
+    {
+        String user = usernameField.getText();
+        String pass = passwordField.getText();
+
+        System.out.println("User: " + user);
+        System.out.println("Pass: " + pass);
+    }
+}
+
