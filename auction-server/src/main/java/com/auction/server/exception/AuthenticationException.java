4881b49b3c77a88ab7ec710a3a4d24c678e8b9a0
NguyenMinhDuc <25020127@vnu.edu.vn>
2026-04-22 09:23:38 +0700

    refractor(server) sua user service, auth exception

--- auction-server/src/main/java/com/auction/server/exception/AuthenticationException.java
+++ auction-server/src/main/java/com/auction/server/exception/AuthenticationException.java
@@ -1,4 +1,4 @@
-package com.auction.server.service;
+package com.auction.server.exception;
 
 public class AuthenticationException extends Exception {
     public AuthenticationException(String message) {
