8e9df07dc69f2dddcbd3f162b82b58b0d9a3f0f5
chisato239 <dthieu2910@gmail.com>
2026-05-06 11:51:49 +0700

    fix login

--- auction-client/src/main/java/com/auction/client/controller/LoginController.java
+++ auction-client/src/main/java/com/auction/client/controller/LoginController.java
@@ -1,25 +1,31 @@
 package com.auction.client.controller;
 
+import com.auction.common.enums.UserRole;
+import javafx.collections.FXCollections;
 import com.auction.client.network.NetworkClient;
 import com.auction.common.message.*;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.stage.Stage;
 
 public class LoginController {
 
+    @FXML private TextField emailField;
+    @FXML private ComboBox<UserRole> roleBox;
     @FXML private TextField usernameField;
     @FXML private PasswordField passwordField;
     @FXML private Label errorLabel;
 
     @FXML
+    public void initialize() {
+        roleBox.setItems(FXCollections.observableArrayList(UserRole.values()));
+    }
+    @FXML
     public void handleLogin() {
 
         String user = usernameField.getText();
         String pass = passwordField.getText();
+        String email = emailField.getText();
+        UserRole role = roleBox.getValue();
 
         // check input
         if (user.isEmpty() || pass.isEmpty()) {
@@ -42,10 +48,13 @@ public class LoginController {
 
     @FXML
     public void handleRegister() {
+
         String user = usernameField.getText();
         String pass = passwordField.getText();
+        String email = emailField.getText();
+        UserRole role = roleBox.getValue();
 
-        if (user.isEmpty() || pass.isEmpty()) {
+        if (user.isEmpty() || pass.isEmpty() || email.isEmpty() || role == null) {
             errorLabel.setText("Nhập đủ thông tin!");
             return;
         }
@@ -54,17 +63,15 @@ public class LoginController {
             NetworkClient client = NetworkClient.getInstance();
             client.connect();
 
-            RegisterRequest req = new RegisterRequest(user, pass, user, null);
+            RegisterRequest req = new RegisterRequest(user, pass, email, role);
             ClientRequest request = new ClientRequest(Action.REGISTER, req);
 
             ClientResponse res = client.sendRequest(request);
 
             errorLabel.setText(res.getMessage());
-            System.out.println("Server: " + res.getMessage());
-
 
         } catch (Exception e) {
-            errorLabel.setText("Không thể kết nối server!");
+            errorLabel.setText("Lỗi kết nối!");
             e.printStackTrace();
         }
     }
