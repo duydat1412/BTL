1e573777685446a50973a05b6ae3541d7f478b35
NguyenMinhDuc <25020127@vnu.edu.vn>
2026-05-09 14:20:22 +0700

    feat(server) add auction endtime scheduler

--- auction-server/src/main/java/com/auction/server/service/AuctionScheduler.java
+++ auction-server/src/main/java/com/auction/server/service/AuctionScheduler.java
@@ -1,4 +1,19 @@
 package com.auction.server.service;
 
+import java.time.LocalDateTime;
+import com.auction.common.entity.Auction;
+import com.auction.common.enums.*;
+
+import java.time.ZoneOffset;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class AuctionScheduler {
+    ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(4);
+
+    public AuctionScheduler(Auction auction){
+        long delay=auction.getEndTime().toInstant(ZoneOffset.ofHours(7)).toEpochMilli()-System.currentTimeMillis();
+        scheduler.schedule(() -> auction.setStatus(AuctionStatus.FINISHED), delay, TimeUnit.MILLISECONDS);
+    }
 }
